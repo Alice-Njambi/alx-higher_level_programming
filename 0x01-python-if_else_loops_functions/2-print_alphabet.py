9d2b2fb73e485dbd309c1487ae76c0d78e24481d
Alice-Njambi <alice.njambi04@gmail.com>
2024-11-16 18:18:01 -0800

    Psydocode error - ommited too many words, and inserted whitespaces where needed.

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(97, 123):  # ASCII values of 'a' to 'z'
-    print("{}".format(chr(i)),end="")  # i is the ASCII value, chr(i) gives the character
+    print("{}".format(chr(i)), end="") #chr(i) gives the character
