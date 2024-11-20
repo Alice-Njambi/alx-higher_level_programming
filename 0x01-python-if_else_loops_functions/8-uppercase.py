9c8ce8bbff4dc812d6e6baf15b638263797f4ea2
Alice-Njambi <alice.njambi04@gmail.com>
2024-11-19 17:22:55 -0800

    Not more than 2 print functions are suppose to be used.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 def uppercase(str):
     # Loop through each character
     for char in str:
         # Convert to uppercase if needed
         if 'a' <= char <= 'z':
-            print(chr(ord(char) - 32), end='')  # Print uppercase letter
+            print("{}".format(chr(ord(char) - 32)), end='')  # Print uppercase letter
         else:
-            print(char, end='')  # Print non-lowercase character.
+            print("{}".format(char), end='')  # Print non-lowercase character
+    
+    print()  # Print new line after loop completes
